3508f1291d099416ece88d8c6cb7f4952728732d
NAZ SUPA <82844614+NAZSUPA@users.noreply.github.com>
2026-04-08 15:00:55 +0300

    Update setupTests.js

--- WEEK3/frontend/src/setupTests.js
+++ WEEK3/frontend/src/setupTests.js
@@ -1,5 +1,4 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+// This import extends the 'expect' library from Jest.
+// It allows you to write tests that check the UI, like:
+// expect(button).toBeDisabled() or expect(header).toBeInTheDocument()
 import '@testing-library/jest-dom';
