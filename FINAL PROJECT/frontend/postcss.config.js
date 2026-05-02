66f3e54606541f440244d7b6bdd750cfe32f2d7a
Aryan Hussen <hussenaryan822@gmail.com>
2026-05-02 07:44:24 -0400

    Add comments to postcss.config.js for clarity
    Added comments to explain the purpose of plugins in the PostCSS configuration.

--- FINAL PROJECT/frontend/postcss.config.js
+++ FINAL PROJECT/frontend/postcss.config.js
@@ -1,6 +1,11 @@
 export default {
+  // Plugins are tools that process your CSS
   plugins: {
+    // This plugin enables Tailwind CSS to process your utility classes
     tailwindcss: {},
+    
+    // This plugin automatically adds "vendor prefixes" (like -webkit- or -moz-) 
+    // to your CSS so that styles look the same on all browsers (Chrome, Safari, Firefox)
     autoprefixer: {},
   },
 }
